50781c21e5b23222d31d1b49d7e347bfa6389018
Numata <numata.stu@gmail.com>
2023-07-14 13:43:59 +0900

    リポジトリにつぶやき検索機能追加

--- tsubuyaki/src/main/java/jp/kobe_u/cs/daikibo/tsubuyaki/repository/TsubuyakiRepository.java
+++ tsubuyaki/src/main/java/jp/kobe_u/cs/daikibo/tsubuyaki/repository/TsubuyakiRepository.java
@@ -5,7 +5,11 @@ import org.springframework.stereotype.Repository;
 
 import jp.kobe_u.cs.daikibo.tsubuyaki.entity.Tsubuyaki;
 
+import java.util.List;
+
 @Repository  
 public interface TsubuyakiRepository extends CrudRepository<Tsubuyaki, Long>{
 
+    // コメントに基づいてつぶやきを検索するカスタムクエリ
+    List<Tsubuyaki> findByComment(String comment);
 }
